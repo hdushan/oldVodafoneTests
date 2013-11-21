f411af5986788632fa1cfd78e9f7993f169775f7
Jacek Hury <jhury@Jaceks-MacBook-Pro.local>
2013-11-21 15:31:31 +1100

    Changed fulfilment service url.

--- app.rb
+++ app.rb
@@ -4,7 +4,12 @@ require "sinatra/namespace"
 require 'haml'
 #require 'newrelic_rpm'
 require_relative 'sinatra_assets'
+require_relative 'lib/fulfilment_service_provider_client'
 
 get '/tnt' do
   haml :main
+end
+
+get '/track/:track_id' do
+  FulfilmentServiceProviderClient.new.get_order_status(params[:track_id])
 end
--- lib/fulfilment_service_provider_client.rb
+++ lib/fulfilment_service_provider_client.rb
@@ -3,13 +3,13 @@ require 'json'
 
 class FulfilmentServiceProviderClient
   include HTTParty
-  base_uri 'http://FulfilmentServiceProvider'
+  base_uri 'http://10.0.2.2:9394'
 
   def get_order_status(order_id)
     responce_json = JSON.parse(self.class.get("/order/#{order_id}").body)
 
     if(responce_json['error'].nil?)
-      # TODO handle correct responce
+      responce_json.to_s
     else
       responce_json['error']
     end
