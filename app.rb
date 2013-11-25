c68578cf70393697b3627ef315907c8858a3c719
James Gregory <james@jagregory.com>
2013-11-25 14:21:56 +1100

    Trace page mockup

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require 'sinatra'
 require "sinatra/namespace"
-
 require 'haml'
-#require 'newrelic_rpm'
 require_relative 'sinatra_assets'
 require_relative 'lib/fulfilment_service_provider_client'
+
+set :public_folder, 'public'
 
 get '/tnt' do
   haml :main
@@ -12,4 +12,12 @@ end
 
 post '/track' do
   FulfilmentServiceProviderClient.new.get_order_status(params[:tracking_id])
+end
+
+get '/trace' do
+  haml :trace
+end
+
+get '/trace.css' do
+  sass :trace
 end
