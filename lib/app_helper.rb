9778a10ffc2d5391d1beb9aa3f0fc8610fb39b83
Jacek Hury <jhury@thoughtworks.com>
2013-12-10 15:55:18 +1100

    [ps/jh] Fixed default msg.

--- lib/app_helper.rb
+++ lib/app_helper.rb
@@ -3,6 +3,6 @@ def error_message
     'FUSION_TIMEOUT' => 'System timeout.',
     'ORDER_NOT_FOUND' => 'Order not found.'
   }
-  error_message.default('Internal error. Sorry')
+  error_message.default = 'Internal error. Sorry'
   error_message
 end
