ac9cd3ab9aa0d0c4bfb3fdfc3daf53b89e687373
Jacek Hury <jhury@thoughtworks.com>
2013-12-10 03:28:59 +0000

    [ps/jh/james] hack for gif to work in IE 8 and 9

--- assets/javascripts/showLoadingImage.js
+++ assets/javascripts/showLoadingImage.js
@@ -1,7 +1,10 @@
 function showLoadingImage(){
-  
+
   $('#loading').show();
   $('#track-form').hide();
+
+  //needed for gif to animate in ie8 and ie9
+  $('#ie-loader').html($('#ie-loader').html());
 
 }
 
