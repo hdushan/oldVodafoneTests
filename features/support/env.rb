54272e1c586400089201d1d6cfa856c9e941ca44
Hans Dushanthakumar <hansrd_98@yahoo.com>
2013-12-18 15:43:44 +1100

    Updated URL of new test env

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,7 +22,7 @@ Dotenv.load(path) if File.exists?(path)
 
 if ENV['RAILS_ENV'] == 'paas-qa'
   puts 'In PAAS QA'
-  Capybara.app_host = "http://trackandtrace-qa.np.syd.services.vodafone.com.au"
+  Capybara.app_host = "http://trackandtrace.qa.np.syd.services.vodafone.com.au"
   Capybara.default_driver    = :poltergeist
   Capybara.run_server = false
 else
