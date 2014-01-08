437b5920b5e8849a8107fc7a70efa99f04836213
Jacek Hury <jhury@thoughtworks.com>
2014-01-08 14:45:09 +1100

    Mega menu client refactor.

--- lib/mega_menu_api_client.rb
+++ lib/mega_menu_api_client.rb
@@ -1,81 +1,13 @@
 require 'httparty'
 require 'json'
+require_relative 'mega_menu_params'
 
 class MegaMenuAPIClient
   include HTTParty
-  base_uri 'https://www.vodafone.com.au/rest'
-
-  def initialize
-    @parameters = {
-        'mobile' => {
-          'header' => {
-            'url'         => 'MobileMenu',
-            'css_names'   => ['vha_vca_mobile_style_css'],
-            'js_names'    => []
-          },
-
-          'footer' => {
-            'url'         => 'MobileFooter',
-            'css_names'   => ['vha_vca_mobile_style'],
-            'js_names'    => []
-          }
-        },
-
-        'desktop' => {
-          'header' => {
-            'url'         => 'Mega_MenuV2',
-            'css_names'   => ['mega_menu_cssv2'],
-            'js_names'    => ['vha_mm_global_jsv2']
-          },
-
-          'footer' => {
-            'url'         => 'Footer',
-            'css_names'   => ['global_vca_css'],
-            'js_names'    => []
-          }
-        }
-    }
-  end
+  base_uri 'https://www.vodafone.com.au/rest/SharedContent?name:contains='
 
   def get_menu(is_mobile_user)
-    if is_mobile_user
-      get_specific_menu(@parameters['mobile'])
-    else
-      get_specific_menu(@parameters['desktop'])
-    end
-  end
-
-  def get_specific_menu(params)
-    get_footer = Thread.new do
-      @footer = get_element(params, 'footer')
-    end
-
-    get_header = Thread.new do
-      @header = get_element(params, 'header')
-    end
-
-    get_footer.join
-    get_header.join
-
-    @header.merge(@footer) { |key, old_val, new_val| old_val + new_val }
-  end
-
-  def get_element(params, element_name)
-    element = params[element_name]
-    begin
-      response_json = get_response_json(element['url'])
-      {
-        "#{element_name}_html" => html(response_json),
-        'css' => element['css_names'].flat_map { |name| uri(find_by_name(response_json, name))},
-        'js' => element['js_names'].flat_map { |name| uri(find_by_name(response_json, name)) }
-      }
-    rescue
-      MegaMenuAPIClient.empty_response
-    end
-  end
-
-  def get_response_json(type)
-    JSON.parse(self.class.get("/SharedContent?name:contains=#{type}").body)
+    get_specific_menu(mega_menu_params[is_mobile_user ? 'mobile' : 'desktop'])
   end
 
   def self.empty_response
@@ -89,15 +21,45 @@ class MegaMenuAPIClient
   end
 
   private
-    def html(json)
+    def get_specific_menu(params)
+      menu = {}
+      %w(footer header).each do |name|
+        t = Thread.new do
+          menu[name] = get_menu_part(params, name)
+        end
+        t.join
+      end
+
+      menu['header'].merge(menu['footer']) { |key, old_val, new_val| old_val + new_val }
+    end
+
+    def get_menu_part(params, menu_part_name)
+      menu_part_params = params[menu_part_name]
+      begin
+        response_json = get_response_json(menu_part_params['url'])
+        {
+            "#{menu_part_name}_html" => extract_html(response_json),
+            'css' => menu_part_params['css_names'].flat_map { |css_file_name| collect_uri(extract_by_name(response_json, css_file_name))},
+            'js' => menu_part_params['js_names'].flat_map { |js_file_name| collect_uri(extract_by_name(response_json, js_file_name)) }
+        }
+      rescue
+        MegaMenuAPIClient.empty_response
+      end
+    end
+
+    def get_response_json(url)
+      JSON.parse(self.class.get(url).body)
+    end
+
+    def collect_uri(list)
+      list.collect { |element| element['uri'] }
+    end
+
+    def extract_html(json)
       json[0]['source']['html']
     end
 
-    def uri(list)
-      list.collect { |res| res['uri'] }
-    end
-
-    def find_by_name(json, name)
+    def extract_by_name(json, name)
       json[0]['resource'].select { |res| res.key?('name') && res['name'] == name }
     end
 end
