5b4035cbc59605c3d535620669dea99b678e7003
Jacek Hury <jhury@thoughtworks.com>
2014-01-09 15:42:08 +1100

    Renamed mobile nav manu div.

--- assets/javascripts/mobileMenu.js
+++ assets/javascripts/mobileMenu.js
@@ -1,11 +1,11 @@
 $(document).ready( function(){
 
   $('.vca-layout-mobile .nav-trigger').on('click', function () {
-    $('.container').toggleClass('search-application-mobile-nav-menu-displayed');
+    $('.container').toggleClass('mobile-nav-menu-displayed');
   });
 
   $('.vca-layout-mobile .mask').on('click', function () {
-    $('.container').toggleClass('search-application-mobile-nav-menu-displayed');
+    $('.container').toggleClass('mobile-nav-menu-displayed');
   });
 });
 
