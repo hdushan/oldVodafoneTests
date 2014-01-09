40761c9af3dd2034fdad93325098bd43f2f009b3
Jacek Hury <jhury@thoughtworks.com>
2014-01-09 15:19:50 +1100

    Added responsive break points. Moved css to size specific folders.

--- assets/javascripts/mobileMenu.js
+++ assets/javascripts/mobileMenu.js
@@ -1,25 +1,11 @@
-//NavMenu = ->
-//    nav_menu_button = $('.vca-layout-mobile .nav-trigger')
-//mask = $('.vca-layout-mobile .mask')
-//
-//display_or_hide_the_nav_menu = ->
-//    search_application_container.toggleClass 'search-application-mobile-nav-menu-displayed'
-//
-//    [nav_menu_button, mask].forEach (element) ->
-//element.on('click', display_or_hide_the_nav_menu)
-
 $(document).ready( function(){
-
-
-
 
   $('.vca-layout-mobile .nav-trigger').on('click', function () {
     $('.container').toggleClass('search-application-mobile-nav-menu-displayed');
   });
 
   $('.vca-layout-mobile .mask').on('click', function () {
-    $('.container').toggleClass('search-application-mobile-nav-menu-displayed'); // TODO finish porting mobile menu
+    $('.container').toggleClass('search-application-mobile-nav-menu-displayed');
   });
-
 });
 
