d9beeca7158b26f0d33e780d6488f15fd8ad3536
Jacek Hury <jhury@thoughtworks.com>
2014-01-14 13:22:26 +1100

    Added mobile menu transition on click

--- assets/javascripts/mobileMenu.js
+++ assets/javascripts/mobileMenu.js
@@ -7,5 +7,13 @@ $(document).ready( function(){
   $('.vca-layout-mobile .mask').on('click', function () {
     $('.container').toggleClass('mobile-nav-menu-displayed');
   });
+
+  $('.vca-layout-mobile .nav-search').on('click', function () {
+    var search_bar_element = $('.vca-layout-mobile .search-bar');
+    debugger;
+    var computed_style = window.getComputedStyle(search_bar_element[0], null);
+    var display_value = computed_style.getPropertyValue('display');
+    search_bar_element[0].style.display = (display_value == 'none' ? 'block' : 'none');
+  });
 });
 
