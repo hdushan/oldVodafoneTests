67a8b3366f30e9de1ead4b4d3575005a5ea36f97
James Gregory <james@jagregory.com>
2014-01-28 00:37:18 +0000

    Use method level rescue

--- lib/fulfilment_client.rb
+++ lib/fulfilment_client.rb
@@ -9,18 +9,16 @@ class FulfilmentClient
   def get_order_status(order_id)
     return { status: 400, 'error' => 'ORDER_ID_EMPTY' } if order_id.empty?
       
-    begin
-      response = self.class.get("/order/#{order_id}")
-      reply = {status: response.code}
-      response_json = JSON.parse(response.body)
-      if response_json['error']
-        reply.store('error', response_json['error'])
-      else
-        reply.store(:body, response_json)
-      end
-      reply
-    rescue Exception => ex
-      { status: 500, 'error' => 'INTERNAL_ERROR', message: ex.message }
+    response = self.class.get("/order/#{order_id}")
+    reply = {status: response.code}
+    response_json = JSON.parse(response.body)
+    if response_json['error']
+      reply.store('error', response_json['error'])
+    else
+      reply.store(:body, response_json)
     end
+    reply
+  rescue Exception => ex
+    { status: 500, 'error' => 'INTERNAL_ERROR', message: ex.message }
   end
 end
