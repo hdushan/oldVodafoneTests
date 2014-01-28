a8253fb6e57fe70269b03e5d5a20240cd9ded6b2
James Gregory <james@jagregory.com>
2014-01-28 00:37:00 +0000

    Make if into a guard

--- lib/fulfilment_client.rb
+++ lib/fulfilment_client.rb
@@ -7,22 +7,20 @@ class FulfilmentClient
   base_uri ENV['FULFILMENT_SERVICE']
 
   def get_order_status(order_id)
-    if(order_id.empty?)
-      { status: 400, 'error' => 'ORDER_ID_EMPTY' }
-    else
-      begin
-        response = self.class.get("/order/#{order_id}")
-        reply = {status: response.code}
-        response_json = JSON.parse(response.body)
-        if response_json['error']
-          reply.store('error', response_json['error'])
-        else
-          reply.store(:body, response_json)
-        end
-        reply
-      rescue Exception => ex
-        { status: 500, 'error' => 'INTERNAL_ERROR', message: ex.message }
+    return { status: 400, 'error' => 'ORDER_ID_EMPTY' } if order_id.empty?
+      
+    begin
+      response = self.class.get("/order/#{order_id}")
+      reply = {status: response.code}
+      response_json = JSON.parse(response.body)
+      if response_json['error']
+        reply.store('error', response_json['error'])
+      else
+        reply.store(:body, response_json)
       end
+      reply
+    rescue Exception => ex
+      { status: 500, 'error' => 'INTERNAL_ERROR', message: ex.message }
     end
   end
 end
