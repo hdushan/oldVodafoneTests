1a9fab5d1a326cf00a370892d3d32694757022d9
James Gregory <james@jagregory.com>
2014-02-03 17:09:05 +1100

    Update pacts to use valid order ids

--- spec/service_providers/fulfilment_client_spec.rb
+++ spec/service_providers/fulfilment_client_spec.rb
@@ -29,20 +29,20 @@ describe FulfilmentClient, :pact => true do
 
   describe 'get_order_details for non existing order id' do
     before do
-      stub_root_resource fulfilment_service_provider, "order with number 123 doesn't exist"
+      stub_root_resource fulfilment_service_provider, "order doesn't exist"
 
       fulfilment_service_provider
-      .given("order with number 123 doesn't exist")
-      .upon_receiving('a request for order status')
-      .with(method: :get, path: '/v1/order/123')
-      .will_respond_with(
-          status: 404,
-          headers: {'Content-Type' => 'application/hal+json'}
-      )
+        .given("order doesn't exist")
+        .upon_receiving('a request for order status')
+        .with(method: :get, path: '/v1/order/VF123NOTFOUND')
+        .will_respond_with(
+            status: 404,
+            headers: {'Content-Type' => 'application/hal+json'}
+        )
     end
 
     it 'should have an error message' do
-      response = fulfilment_client.get_order_details('123')
+      response = fulfilment_client.get_order_details('VF123NOTFOUND')
 
       expect(response.has_error?).to be_true
       expect(response.error_message).to eq('That order ID was not found. Please, check that you typed it correctly.')
@@ -52,36 +52,36 @@ describe FulfilmentClient, :pact => true do
   describe 'get_order_details for existing order id' do
     let(:response_body) {
       {
-          'tracking_status' => 'IN PROGRESS',
-          'ordered_date' => '2013-07-31',
-          'consignment_number' => 'cn123',
-          'items' => [
-              {
-                  'description' => 'phone'
-              },
-              {
-                  'description' => 'sim'
-              }
-          ]
+        'tracking_status' => 'IN PROGRESS',
+        'ordered_date' => '2013-07-31',
+        'consignment_number' => 'cn123',
+        'items' => [
+          {
+            'description' => 'phone'
+          },
+          {
+            'description' => 'sim'
+          }
+        ]
       }
     }
 
     before do
-      stub_root_resource fulfilment_service_provider, 'order with number 456 exists and completed'
+      stub_root_resource fulfilment_service_provider, 'order exists and completed'
 
       fulfilment_service_provider
-      .given('order with number 456 exists and completed')
-      .upon_receiving('a request for order status')
-      .with(method: :get, path: '/v1/order/456')
-      .will_respond_with(
+        .given('order exists and completed')
+        .upon_receiving('a request for order status')
+        .with(method: :get, path: '/v1/order/VF456')
+        .will_respond_with(
           status: 200,
           headers: {'Content-Type' => 'application/hal+json'},
           body: response_body
-      )
+        )
     end
 
     it 'should return an order status' do
-      response = fulfilment_client.get_order_details('456')
+      response = fulfilment_client.get_order_details('VF456')
 
       expect(response.has_error?).to be_false
       expect(response.status_message).to match /in progress/
@@ -94,17 +94,17 @@ describe FulfilmentClient, :pact => true do
       stub_root_resource fulfilment_service_provider, 'an unexpected error in fusion'
 
       fulfilment_service_provider
-      .given('an unexpected error in fusion')
-      .upon_receiving('a request for order status')
-      .with(method: :get, path: '/v1/order/503')
-      .will_respond_with(
+        .given('an unexpected error in fusion')
+        .upon_receiving('a request for order status')
+        .with(method: :get, path: '/v1/order/VF503')
+        .will_respond_with(
           status: 503,
           headers: {'Content-Type' => 'application/hal+json'}
-      )
+        )
     end
 
     it 'should return a generic error message' do
-      response = fulfilment_client.get_order_details('503')
+      response = fulfilment_client.get_order_details('VF503')
 
       expect(response.has_error?).to be_true
       expect(response.error_message).to match /Service Unavailable/
