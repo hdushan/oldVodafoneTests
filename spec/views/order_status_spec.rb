265c89d09e6ba0b1f538ad7dc6a8ed88aadb9bda
Mary-Anne Cosgrove <mcosgro@thoughtworks.com>
2014-02-05 12:09:03 +1100

    [FUL-90][MAC] Escape special characters in description strings.[FUL-91] Change label for Order Number.

--- spec/views/order_status_spec.rb
+++ spec/views/order_status_spec.rb
@@ -8,14 +8,15 @@ describe 'order_status.haml' do
                         order_number: 'VF123MULTILINES', status_heading: 'In Progress', status_message: 'Your order is in progress.',
                         items: [
                             {item_quantity: "1", description: 'Samsung Galaxy'},
-                            {item_quantity: "2", description: 'iPhone 5C'}
+                            {item_quantity: "2", description: 'iPhone 5C'},
+                            {item_quantity: "3", description: %q{Hooray for special characters! ./?/<h1>!@#$%^&*(;:)_+''=-,\"`}}
                         ])
       render("/views/order_status.haml", :details => @details)
       #puts response
     end
 
     it 'should include the order number' do
-      expect(page.find('.order-number-label')).to have_content('Order Status:')
+      expect(page.find('.order-number-label')).to have_content('Order Status for order number:')
       expect(page.find('.order-number')).to have_content('VF123MULTILINES')
     end
 
@@ -26,10 +27,14 @@ describe 'order_status.haml' do
 
     it 'should have order details and order detail heading' do
       items = page.all('.item')
-      expect(items.size).to be(2)
+      expect(items.size).to be(3)
       expect(items.first).to have_content('1 x Samsung Galaxy')
-      expect(items.last).to have_content('2 x iPhone 5C')
+      expect(items[1]).to have_content('2 x iPhone 5C')
       expect(page.find('.order-details-heading')).to have_content('Order Details')
+    end
+
+    it 'should HTML escape the special characters' do
+      response.should include(%q{Hooray for special characters! ./?/&lt;h1&gt;!@#$%^&amp;*(;:)_+''=-,\&quot;`})
     end
 
   end
@@ -45,7 +50,7 @@ describe 'order_status.haml' do
     end
 
     it 'should include the order number' do
-      expect(page.find('.order-number-label')).to have_content('Order Status:')
+      expect(page.find('.order-number-label')).to have_content('Order Status for order number:')
       expect(page.find('.order-number')).to have_content('1-123INPROGRESS')
     end
 
@@ -59,4 +64,5 @@ describe 'order_status.haml' do
       page.should have_no_selector('.order-details-heading')
     end
   end
+
 end
