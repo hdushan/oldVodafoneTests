ba252614199095ae171c0bc67795368764777b54
yongpeishi <yongpeishi@gmail.com>
2014-02-10 16:19:50 +1100

    [ps] display generic message if having error when request to AusPost

---
+++ spec/views/_shipping_info.haml_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe '_tracking_info.haml' do
+
+  let(:tracking_info) { {} }
+  before do
+    render("/views/_shipping_info.haml", :tracking => tracking_info)
+  end
+
+  context 'has no error in getting tracking information' do
+    let(:tracking_info) { {'foo' => 'bar'} }
+
+    it 'should display tracking information' do
+      expect(page.find('.tracking-info-heading')).to have_content('Shipping Details')
+      expect(page).to have_no_selector('.tracking-info-generic-msg')
+    end
+  end
+
+  context 'has error in getting tracking information' do
+    let(:tracking_info) { {'error' => 'kitten'} }
+
+    it 'should display tracking information' do
+      expect(page.find('.tracking-info-heading')).to have_content('Shipping Details')
+      expect(page.find('.tracking-info-generic-msg')).to have_content('Your order has been shipped.')
+    end
+  end
+
+end
