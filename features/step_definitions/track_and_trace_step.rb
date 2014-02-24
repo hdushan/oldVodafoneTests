fc735961ff0e6129ad5ff2e0da4e9d30d9ed6b39
Hans Dushanthakumar <hansrd_98@yahoo.com>
2014-02-24 15:01:36 +1100

    Made test case consistent with statuses

--- features/step_definitions/track_and_trace_step.rb
+++ features/step_definitions/track_and_trace_step.rb
@@ -63,8 +63,7 @@ Then(/^I should see the right count, description and status for each item$/) do
   expected_descriptions = ['iPhone 5C 16GB Blue',
               'iPhone 5C 16GB Gold',
               'iPhone 5C 16GB Silver',
-              "iPhone 5C 16GB Grey",
-              "iPhone 5C 16GB Black"]
+              "iPhone 5C 16GB Grey"]
   actual = page.all('.item').map { |elem| elem.text }
   expect(actual).to match_array expected_descriptions
   expected_counts = ['1 x', '2 x', '3 x', "4 x", "5 x"]
@@ -73,8 +72,7 @@ Then(/^I should see the right count, description and status for each item$/) do
   expected_statuses = ['Cancelled', 
              'Shipped', 
              'Cancelled', 
-             "Shipped", 
-             "On Backorder"]
+             "Shipped"]
   actual_statuses = page.all('.item-status').map { |elem| elem.text }
   expect(actual_statuses).to match_array expected_statuses
 end
