f87d0bb4e286808556bc23862a1aaa182822dfa1
Mary-Anne Cosgrove <mcosgro@thoughtworks.com>
2014-03-14 13:54:30 +1100

    [FUL-94][MAC] Get the right set of stylesheets and js into ie8, ie9 and all the rest.

--- lib/sinatra_assets.rb
+++ lib/sinatra_assets.rb
@@ -8,10 +8,10 @@ module Assets
       set :public_folder, 'public'
 
       assets {
-        serve '/fonts',  from: 'assets/fonts'
-        serve '/js',     from: 'assets/javascripts'        # Default
-        serve '/css',    from: 'assets/stylesheets'       # Default
-        serve '/images', from: 'assets/images'    # Default
+        serve '/fonts', from: 'assets/fonts'
+        serve '/js', from: 'assets/javascripts' # Default
+        serve '/css', from: 'assets/stylesheets' # Default
+        serve '/images', from: 'assets/images' # Default
 
         # The second parameter defines where the compressed version will be served.
         # (Note: that parameter is optional, AssetPack will figure it out.)
@@ -22,16 +22,24 @@ module Assets
           '/js/*.js'
         ]
 
-        css :application, '/css/application.css', [
-          '/css/*.css'
+        js :ie8, '/js/ie8.js', [
+          'js/vendor/html5shiv.js'
         ]
 
-        css :ie, '/css/ie.css', [
-          '/css/ie/ie.css'
+        css :main, '/css/application.css', [
+          '/css/app.css', '/css/loader.css'
         ]
 
-        js_compression  :closure    # :jsmin | :closure | :uglify
-        css_compression :sass   # :simple | :sass | :yui | :sqwish
+        css :ie9, '/css/ie9only.css', [
+          '/css/app.css', '/css/ieloader.css'
+        ]
+
+        css :ie8, '/css/ie8only.css', [
+          '/css/ie8.css', '/css/ieloader.css'
+        ]
+
+        js_compression :closure # :jsmin | :closure | :uglify
+        css_compression :sass # :simple | :sass | :yui | :sqwish
       }
     end
   end
