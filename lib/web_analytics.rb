7839c5f375e2ea63c091aee4a228d26b31e0191b
Evgeny Dudin <nagliyvred@gmail.com>
2014-03-17 15:50:45 +1100

    added web analytics data on page load

---
+++ lib/web_analytics.rb
@@ -0,0 +1,41 @@
+
+
+class WebAnalytics
+
+  def initialize(tracking_id, fulfilment_response)
+    @tracking_id = tracking_id
+    @fulfilment_response = fulfilment_response
+  end
+
+  def to_json
+    status_details = {
+        'trackingId' => @tracking_id,
+        'orderStatus' => order_status_string
+    }
+    status_details['auspostStatus'] = tracking_status_string unless trackable_orders.empty?
+    [ 'tnt', status_details ]
+  end
+
+  def error_json
+    [ 'tnt', { 'trackingId' => @tracking_id, 'orderStatus' => 'error' } ]
+  end
+private
+  def tracking_status_string
+    return 'error' if @fulfilment_response.has_error?
+
+    status_string = trackable_orders.map do |order|
+      order.auspost_status_heading
+    end
+    status_string.join(',').downcase.gsub(' ', '_')
+  end
+
+  def trackable_orders
+    @fulfilment_response.orders.select { |order| order.tracking }
+  end
+
+  def order_status_string
+    return 'error' if @fulfilment_response.has_error?
+    status_string = @fulfilment_response.orders.map { |order| order.tracking_status }.join(',')
+    status_string.downcase.gsub(' ', '_')
+  end
+end
